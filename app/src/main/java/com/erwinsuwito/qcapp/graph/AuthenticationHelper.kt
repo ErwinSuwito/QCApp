bf52300ffcb7873f17225926f5f2f2f1cd078e33
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-03-20 19:02:36 +0700

    Added AuthenticationHelper.kt and GraphHelper.kt, finished login flow

---
+++ app/src/main/java/com/erwinsuwito/qcapp/graph/AuthenticationHelper.kt
@@ -0,0 +1,69 @@
+package com.erwinsuwito.qcapp.graph
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import com.erwinsuwito.qcapp.R
+import com.microsoft.identity.client.AuthenticationCallback
+import com.microsoft.identity.client.IPublicClientApplication
+import com.microsoft.identity.client.ISingleAccountPublicClientApplication
+import com.microsoft.identity.client.PublicClientApplication
+import com.microsoft.identity.client.exception.MsalException
+
+// Singleton class - the app only needs a single instance of PublicClientApplication
+public class AuthenticationHelper private constructor(ctx: Context) {
+
+    private var pca: ISingleAccountPublicClientApplication? = null
+    private val scopes = arrayOf("User.Read", "Calendars.Read")
+
+    init {
+        PublicClientApplication.createSingleAccountPublicClientApplication(
+                ctx.applicationContext,
+                R.raw.msal_config,
+                object : IPublicClientApplication.ISingleAccountApplicationCreatedListener {
+                    override fun onCreated(application: ISingleAccountPublicClientApplication?) {
+                        pca = application
+                    }
+
+                    override fun onError(exception: MsalException?) {
+                        Log.e("AUTHHELPER", "Error creating MSAL application", exception)
+                    }
+                })
+    }
+
+    companion object {
+        private var INSTANCE: AuthenticationHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): AuthenticationHelper =
+                INSTANCE ?: AuthenticationHelper(ctx).apply {
+                    INSTANCE = this
+                }
+
+        // Version called from fragments. Does not create an instance if one doesn't exist
+        @Synchronized
+        fun getInstance(): AuthenticationHelper = INSTANCE ?: throw IllegalStateException(
+                "AuthenticationHelper has not been initialized from MainActivity"
+        )
+    }
+
+    fun acquireTokenInteractively(activity: Activity, callback: AuthenticationCallback) =
+            pca!!.signIn(activity, "", scopes, callback)
+
+    fun acquireTokenSilently(callback: AuthenticationCallback) = pca!!.apply {
+        // Get the authority from MSAL config
+        val authority = configuration.defaultAuthority.authorityURL.toString()
+        Log.d("AUTHHELPER", "acquireTokenSilently: authority=$authority")
+        acquireTokenSilentAsync(scopes, authority, callback)
+    }
+
+    fun signOut() = pca!!.signOut(object : ISingleAccountPublicClientApplication.SignOutCallback {
+        override fun onSignOut() {
+            Log.d("AUTHHELPER", "Signed out")
+        }
+
+        override fun onError(exception: MsalException) {
+            Log.d("AUTHHELPER", "MSAL error signing out", exception)
+        }
+    })
+}
--- app/src/main/java/com/erwinsuwito/qcapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/erwinsuwito/qcapp/ui/login/LoginActivity.kt
@@ -1,23 +1,150 @@
 package com.erwinsuwito.qcapp.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
-import android.view.WindowManager
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.erwinsuwito.qcapp.MainActivity
 import com.erwinsuwito.qcapp.R
+import com.erwinsuwito.qcapp.graph.GraphHelper
+import com.erwinsuwito.qcapp.graph.AuthenticationHelper
+import com.microsoft.graph.concurrency.ICallback
+import com.microsoft.graph.core.ClientException
+import com.microsoft.graph.models.extensions.User
+import com.microsoft.identity.client.AuthenticationCallback
+import com.microsoft.identity.client.IAuthenticationResult
+import com.microsoft.identity.client.exception.MsalClientException
+import com.microsoft.identity.client.exception.MsalException
+import com.microsoft.identity.client.exception.MsalServiceException
+import com.microsoft.identity.client.exception.MsalUiRequiredException
+
+private const val SAVED_IS_SIGNED_IN = "isSignedIn"
 
 class LoginActivity : AppCompatActivity() {
+    private var isSignedIn = false
+    private lateinit var authHelper: AuthenticationHelper
+    private var attemptInteractiveSignIn: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // This is used to hide the status bar and make the login screen as a full screen activity.
-        // It is deprecated in the API level 30.
-        // TO-DO: Update with newer code
-        /*
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-        */
+        authHelper = AuthenticationHelper.getInstance(applicationContext)
+
+        val loginButton: Button = findViewById(R.id.button)
+        loginButton.setOnClickListener()
+        {
+            signIn()
+        }
+
+        findViewById<ProgressBar>(R.id.progressBar).isVisible = true
+
+        savedInstanceState?.apply {
+            isSignedIn = getBoolean(SAVED_IS_SIGNED_IN)
+        } ?: run {
+            val handler = Handler().postDelayed({
+                doSilentSignIn()
+            }, 2000)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) = with(outState) {
+        super.onSaveInstanceState(this)
+        putBoolean(SAVED_IS_SIGNED_IN, isSignedIn)
+    }
+
+    private fun signIn() {
+        doSilentSignIn()
+    }
+
+    // Silently sign in - used if there is already a
+    // user account in the MSAL cache
+    private fun doSilentSignIn() = authHelper.acquireTokenSilently(getAuthCallback())
+
+    // Prompt the user to sign in
+    private fun doInteractiveSignIn() =
+            authHelper.acquireTokenInteractively(this, getAuthCallback())
+
+    // Handles the authentication result
+    private fun getAuthCallback(): AuthenticationCallback = object : AuthenticationCallback {
+        override fun onSuccess(authenticationResult: IAuthenticationResult?) =
+                authenticationResult?.accessToken.let {
+                    // Log the token for debug purposes
+                    Log.d("AUTH", String.format("Access token: %s", it))
+
+                    // Get Graph client and get user
+                    GraphHelper.getInstance().getUser(it!!, getUserCallback())
+
+                    isSignedIn = true;
+                }
+
+        override fun onCancel() {
+            // User canceled the authentication
+            Log.d("AUTH", "Authentication canceled")
+        }
+
+        override fun onError(exception: MsalException?): Unit = with(exception) {
+            // Check the type of "this" exception and handle appropriately
+            if (this is MsalUiRequiredException) {
+                Log.d("AUTH", "Interactive login required")
+                if (attemptInteractiveSignIn) doInteractiveSignIn()
+            } else if (this is MsalClientException) {
+                if (errorCode == "no_current_account") {
+                    Log.d("AUTH", "No current account, interactive login required")
+                    if (attemptInteractiveSignIn) doInteractiveSignIn()
+                } else {
+                    // Exception inside MSAL, more info inside MsalError.java
+                    Log.e("AUTH", "Client error authenticating", exception)
+                }
+            } else if (this is MsalServiceException) {
+                // Exception when communicating with the auth server, likely config issue
+                Log.e("AUTH", "Service error authenticating", exception)
+            }
+
+            findViewById<LinearLayout>(R.id.linearLayout).isVisible = true
+            findViewById<Button>(R.id.button).isVisible = true
+            findViewById<ProgressBar>(R.id.progressBar).isVisible = false
+
+            attemptInteractiveSignIn = true
+        }
+    }
+
+    private fun getUserCallback(): ICallback<User> = object : ICallback<User> {
+        override fun success(result: User?) {
+            result?.apply {
+                Log.d("AUTH", "User: $displayName")
+            }
+            updateUI()
+        }
+
+        override fun failure(ex: ClientException?) {
+            Log.e("AUTH", "Error getting /me", ex)
+            updateUI()
+        }
+
+        private fun updateUI() = runOnUiThread {
+            if (isSignedIn)
+            {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else
+            {
+                attemptInteractiveSignIn = true
+                val layout: LinearLayout = findViewById(R.id.linearLayout)
+                layout.isVisible = true
+                val loginButton: Button = findViewById(R.id.button)
+                loginButton.isVisible = true
+            }
+        }
     }
 }
