08c146ff06339c1e9a5e39e70bca8e65d7c4a6a4
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-04-22 20:05:31 +0700

    Completed codes to show FABs in QcAdmin page

--- app/src/main/java/com/erwinsuwito/qcapp/ui/qcadmin/QcAdminFragment.kt
+++ app/src/main/java/com/erwinsuwito/qcapp/ui/qcadmin/QcAdminFragment.kt
@@ -1,24 +1,32 @@
 package com.erwinsuwito.qcapp.ui.qcadmin
 
 import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import com.erwinsuwito.qcapp.R
+import kotlinx.android.synthetic.main.qcadmin_fabs_layout.*
 
-class QcAdminFragment : Fragment() {
+
+class QcAdminFragment : Fragment(), View.OnClickListener {
 
     companion object {
         fun newInstance() = QcAdminFragment()
     }
 
     private lateinit var viewModel: QcAdminViewModel
+    var isOpen = false
+    private var fab_open: Animation? = null
+    private var fab_close:Animation? = null
+    private var fab_clock:Animation? = null
+    private var fab_anticlock:Animation? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -40,6 +48,52 @@ class QcAdminFragment : Fragment() {
             /// Navigation.findNavController(root).navigate(R.id.action_fragment_qc_admin_to_qcAdminNotAllowed)
             requireView().findNavController().navigate(R.id.action_fragment_qc_admin_to_noPermissionsFragment)
         }
+
+        fab_open = AnimationUtils.loadAnimation(this.context, R.anim.fab_open)
+        fab_close = AnimationUtils.loadAnimation(this.context, R.anim.fab_close)
+        fab_clock = AnimationUtils.loadAnimation(this.context, R.anim.fab_rotate_clock)
+        fab_anticlock = AnimationUtils.loadAnimation(this.context, R.anim.fab_rotate_anticlock)
+
+        rootFab.setOnClickListener(this)
     }
 
+    override fun onClick(v: View?)
+    {
+        if (v != null)
+        {
+            when (v.id)
+            {
+                R.id.rootFab -> {
+                    if (isOpen)
+                    {
+                        textview_class.visibility = View.INVISIBLE
+                        textview_projector.visibility = View.INVISIBLE
+                        textview_tasks.visibility = View.INVISIBLE
+                        addClassFab.startAnimation(fab_close)
+                        addProjectorFab.startAnimation(fab_close)
+                        addTasksFab.startAnimation(fab_close)
+                        addClassFab.isClickable = false
+                        addProjectorFab.isClickable = false
+                        addTasksFab.isClickable = false
+                        rootFab.startAnimation(fab_anticlock)
+                        isOpen = false
+                    }
+                    else
+                    {
+                        textview_class.visibility = View.VISIBLE
+                        textview_projector.visibility = View.VISIBLE
+                        textview_tasks.visibility = View.VISIBLE
+                        addClassFab.startAnimation(fab_open)
+                        addProjectorFab.startAnimation(fab_open)
+                        addTasksFab.startAnimation(fab_open)
+                        addClassFab.isClickable = true
+                        addProjectorFab.isClickable = true
+                        addTasksFab.isClickable = true
+                        rootFab.startAnimation(fab_clock)
+                        isOpen = true
+                    }
+                }
+            }
+        }
+    }
 }
