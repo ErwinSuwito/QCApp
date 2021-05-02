4f1cf394066686c34f6c60fd6df97f835c4bc482
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-05-02 12:39:34 +0700

    Copied contents from class detail fragment to class detail activity

---
+++ app/src/main/java/com/erwinsuwito/qcapp/ui/classrooms/ClassDetailActivity.kt
@@ -0,0 +1,61 @@
+package com.erwinsuwito.qcapp.ui.classrooms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.erwinsuwito.qcapp.R
+import com.erwinsuwito.qcapp.ui.issues.ClassIssueListFragment
+import com.google.android.material.tabs.TabLayout
+
+class ClassDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class_detail)
+
+        var tab_viewpager = findViewById<ViewPager>(R.id.class_viewpager)
+        var tab_tablayout = findViewById<TabLayout>(R.id.tabLayout)
+
+        setupViewPager(tab_viewpager)
+        tab_tablayout.setupWithViewPager(tab_viewpager)
+    }
+
+    private fun setupViewPager(viewpager: ViewPager) {
+        var adapter: ViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        adapter.addFragment(ClassInfoFragment("D-08-09"), "Info")
+        adapter.addFragment(ClassIssueListFragment("D-08-09"), "Issues")
+
+        viewpager.setAdapter(adapter)
+    }
+
+    class ViewPagerAdapter : FragmentPagerAdapter {
+        private final var fragmentList1: ArrayList<Fragment> = ArrayList()
+        private final var fragmentTitleList1: ArrayList<String> = ArrayList()
+
+        public constructor(supportFragmentManager: FragmentManager)
+                : super(supportFragmentManager)
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList1.get(position)
+        }
+
+        @Nullable
+        override fun getPageTitle(position: Int): CharSequence {
+            return fragmentTitleList1.get(position)
+        }
+
+        override fun getCount(): Int {
+            return fragmentList1.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragmentList1.add(fragment)
+            fragmentTitleList1.add(title)
+        }
+
+    }
+}
