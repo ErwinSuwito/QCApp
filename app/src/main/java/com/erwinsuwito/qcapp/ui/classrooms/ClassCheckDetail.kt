6d62bc4f396a03643e74ca1a0b94461d40cebe0b
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-05-03 11:25:38 +0700

    Added empty class check details activity

---
+++ app/src/main/java/com/erwinsuwito/qcapp/ui/classrooms/ClassCheckDetail.kt
@@ -0,0 +1,12 @@
+package com.erwinsuwito.qcapp.ui.classrooms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erwinsuwito.qcapp.R
+
+class ClassCheckDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class_check_detail)
+    }
+}
