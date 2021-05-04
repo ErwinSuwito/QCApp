d217ee3bc8fb48e2ce5c8a2ce629e107ee966ff7
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-05-04 13:44:22 +0700

    Added code to show required fragments in qc page. Removed the qc admin fab layout

--- app/src/main/java/com/erwinsuwito/qcapp/ui/qc/QcFragment.kt
+++ app/src/main/java/com/erwinsuwito/qcapp/ui/qc/QcFragment.kt
@@ -25,19 +25,11 @@ import com.erwinsuwito.qcapp.ui.issues.ClassIssueListFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.microsoft.fluentui.util.activity
-import kotlinx.android.synthetic.main.qcadmin_fabs_layout.*
 import java.time.LocalDateTime
 
-class QcFragment : Fragment(), View.OnClickListener {
+class QcFragment : Fragment() {
 
-
-    //TO-DO: Complete the codes to show or hide FABs
     private lateinit var qcFragmentViewModel: QcFragmentViewModel
-    var isOpen = false
-    private var fab_open: Animation? = null
-    private var fab_close: Animation? = null
-    private var fab_clock: Animation? = null
-    private var fab_anticlock: Animation? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -48,26 +40,16 @@ class QcFragment : Fragment(), View.OnClickListener {
                 ViewModelProvider(this).get(QcFragmentViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_qc, container, false)
 
+        /*
+        TO-DO: Move this code to the other fragments
         var sharedPreferences = activity?.getSharedPreferences("prefs", Context.MODE_PRIVATE)
-
-        fab_open = AnimationUtils.loadAnimation(this.context, R.anim.fab_open)
-        fab_close = AnimationUtils.loadAnimation(this.context, R.anim.fab_close)
-        fab_clock = AnimationUtils.loadAnimation(this.context, R.anim.fab_rotate_clock)
-        fab_anticlock = AnimationUtils.loadAnimation(this.context, R.anim.fab_rotate_anticlock)
-
-        val rootFab = root.findViewById<FloatingActionButton>(R.id.rootFab)
-        val addTasksFab = root.findViewById<FloatingActionButton>(R.id.addTasksFab)
-        val addClassFab = root.findViewById<FloatingActionButton>(R.id.addClassFab)
 
         if (sharedPreferences!!.getString("usr_role", "Technical Assistant") == "Board Member")
         {
             rootFab.isEnabled = true
             rootFab.visibility = View.VISIBLE
         }
-
-        rootFab.setOnClickListener(this)
-        addTasksFab.setOnClickListener(this)
-        addClassFab.setOnClickListener(this)
+         */
 
         var tab_viewpager = root.findViewById<ViewPager>(R.id.qc_viewpager)
         var tab_tablayout = root.findViewById<TabLayout>(R.id.qc_tabLayout)
@@ -78,44 +60,12 @@ class QcFragment : Fragment(), View.OnClickListener {
         return root
     }
 
-    override fun onClick(v: View?)
-    {
-        if (v != null)
-        {
-            when (v.id)
-            {
-                R.id.rootFab -> {
-                    if (isOpen)
-                    {
-                        textview_class.visibility = View.INVISIBLE
-                        textview_tasks.visibility = View.INVISIBLE
-                        addClassFab.startAnimation(fab_close)
-                        addTasksFab.startAnimation(fab_close)
-                        addClassFab.isClickable = false
-                        addTasksFab.isClickable = false
-                        rootFab.startAnimation(fab_anticlock)
-                        isOpen = false
-                    }
-                    else
-                    {
-                        textview_class.visibility = View.VISIBLE
-                        textview_tasks.visibility = View.VISIBLE
-                        addClassFab.startAnimation(fab_open)
-                        addTasksFab.startAnimation(fab_open)
-                        addClassFab.isClickable = true
-                        addTasksFab.isClickable = true
-                        rootFab.startAnimation(fab_clock)
-                        isOpen = true
-                    }
-                }
-            }
-        }
-    }
 
     private fun setupViewPager(viewpager: ViewPager) {
         var adapter: ViewPagerAdapter = ViewPagerAdapter(childFragmentManager)
 
-        adapter.addFragment(ClassIssueListFragment("D-08-09"), "Issues")
+        adapter.addFragment(ClassListFragment(), "Classes")
+        adapter.addFragment(IssueListFragment(), "Issues")
 
         viewpager.setAdapter(adapter)
     }
