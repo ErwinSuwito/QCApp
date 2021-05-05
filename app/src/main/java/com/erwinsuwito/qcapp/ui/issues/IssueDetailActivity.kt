232cc2c7fe01da71b7ced48a24cf48ed8eeb1977
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-05-05 12:16:32 +0700

    Added initial design for issue detail activity

---
+++ app/src/main/java/com/erwinsuwito/qcapp/ui/issues/IssueDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.erwinsuwito.qcapp.ui.issues
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erwinsuwito.qcapp.R
+
+class IssueDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_issue_detail)
+    }
+}
