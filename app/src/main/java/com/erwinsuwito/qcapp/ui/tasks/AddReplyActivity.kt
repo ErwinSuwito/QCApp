afa51ed3b4b9d4fb291d06a1a1095f3b20094567
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-05-05 15:19:39 +0700

    Added UI to reply to issues and tasks

---
+++ app/src/main/java/com/erwinsuwito/qcapp/ui/tasks/AddReplyActivity.kt
@@ -0,0 +1,11 @@
+package com.erwinsuwito.qcapp.ui.tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddReplyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_reply)
+    }
+}
