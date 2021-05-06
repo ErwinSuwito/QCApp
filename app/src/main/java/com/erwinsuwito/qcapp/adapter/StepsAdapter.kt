f4673891b469d079976f1ffb82de21add7f82296
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-05-07 06:52:28 +0700

    Completed code for StepsAdapter.kt

--- app/src/main/java/com/erwinsuwito/qcapp/adapter/StepsAdapter.kt
+++ app/src/main/java/com/erwinsuwito/qcapp/adapter/StepsAdapter.kt
@@ -10,12 +10,17 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.erwinsuwito.qcapp.R
 import com.erwinsuwito.qcapp.model.Steps
+import com.microsoft.fluentui.persona.AvatarView
 
 class StepsAdapter(private val context: Context, private val dataset: List<Steps>, private val onClick: (Steps) -> Unit)
     : ListAdapter<Steps, StepsAdapter.ItemViewHolder>(stepsItemDiffCallback)
 {
     class ItemViewHolder(private val view: View, val onClick: (Steps) -> Unit) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.class_name)
+        val authorName: TextView = view.findViewById(R.id.reply_author)
+        val authorAvatar: AvatarView = view.findViewById(R.id.reply_avatar)
+        val replyContent: TextView = view.findViewById(R.id.reply_replyContent)
+        val addedDate: TextView = view.findViewById(R.id.reply_dateAdded)
+
         private var currentItem: Steps? = null
 
         init {
@@ -34,7 +39,7 @@ class StepsAdapter(private val context: Context, private val dataset: List<Steps
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
-                .inflate(R.layout.class_list_item, parent, false)
+                .inflate(R.layout.troubleshoot_step_list_item, parent, false)
 
         return ItemViewHolder(adapterLayout, onClick)
     }
@@ -45,7 +50,11 @@ class StepsAdapter(private val context: Context, private val dataset: List<Steps
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text = item.stepId.toString()
+        holder.authorAvatar.name = item.authorName
+        holder.authorName.text = item.authorName
+        holder.replyContent.text = item.content
+        holder.addedDate.text = item.addedOn.toDate().toLocaleString()
+
         holder.bind(item)
     }
 }
